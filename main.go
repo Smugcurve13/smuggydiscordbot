e35302efe31e2dde7852b982c4ac6bb500a134d6
Sambhav Soni <sambhav.soni@compunnel.com>
2026-04-20 13:25:50 +0530

    stats function works

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 )
 
 
-
 func main() {
 	fmt.Println(time.Now())
 	err := godotenv.Load()
@@ -86,16 +85,43 @@ func getStats() (string, error) {
 	
 	defer client.Close()
 	fmt.Println("SSH CONNECTED !!")
-	session, err2 := client.NewSession()
+
+	ram_cmd := "free -m"
+	cpu_cmd := `grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$3+$4+$5)} END {print usage"%"}'`
+	disk_cmd := "df -h /"
+	temp_cmd := `sensors | grep "Package id 0" | awk '{print $4}'`
+	
+	ram , err1 := runCommand(client, ram_cmd)
+	if err1 != nil {
+		fmt.Println("Error while excecuting RAM stats command")
+	}
+	cpu , err2 := runCommand(client, cpu_cmd)
 	if err2 != nil {
-		fmt.Printf("Error in Creating New Session %v", err2)
+		fmt.Println("Error while excecuting CPU stats command")
+	}
+	disk , err3 := runCommand(client, disk_cmd)
+	if err3 != nil {
+		fmt.Println("Error while excecuting DISK stats command")
+	}
+	temp , err4 := runCommand(client, temp_cmd)
+	if err4 != nil {
+		fmt.Println("Error while excecuting TEMPERATURE stats command")
+	}
+	result := fmt.Sprintf("```\nSmuggyServer Stats\n\nRAM:\n%v\nCPU:\n%v\nDISK:\n%v\nTEMPERATURE:\n%v\n```", ram, cpu, disk, temp)
+	fmt.Println(result)
+	return result , nil
+}
+
+func runCommand(client *ssh.Client, command string) (string, error) {
+	session, err := client.NewSession()
+	if err != nil {
+		fmt.Printf("Error in Creating New Session : %v", err)
 	}
 	defer session.Close()
-	output, err3 := session.Output("free -m")
+	output, err3 := session.Output(command)
 	if err3 != nil {
-		fmt.Printf("Error in Running Command %v", err3)
+		fmt.Printf("Error in Running Command : %v", err3)
 	}
 	str_output := string(output)
-	fmt.Printf("Stats are : %v", str_output)
-	return str_output , nil
+	return str_output, nil
 }
