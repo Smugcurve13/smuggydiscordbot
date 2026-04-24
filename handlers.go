1af73cf0f09cf72e6781f180d37b8e5308acecc0
Smugcurve13 <sambhavsoni14@gmail.com>
2026-04-24 23:14:50 +0530

    added a message parser func

--- handlers.go
+++ handlers.go
@@ -15,13 +15,24 @@ func MessageHandlerv3(session *discordgo.Session, message *discordgo.MessageCrea
 	}
 	fmt.Println("Message Received")
 	msg := message.Content
-	if strings.HasPrefix(msg, "!") {
-		msg := strings.TrimPrefix(msg, "!")
-		user_command, argument, _ := strings.Cut(msg, " ")
+	user_command, argument, is_command := messageParser(msg)
+	if is_command {
 		if cmd_func, exists := COMMAND_REGISTRY[user_command]; exists {
 			output := cmd_func(message, argument)
 			output = fmt.Sprintf("```%s```",output)
 			session.ChannelMessageSend(message.ChannelID, output)
 		}
+	}
+}
+
+func messageParser(msg string) (string, string, bool) {
+	is_command := false
+	if strings.HasPrefix(msg, "!") {
+		msg := strings.TrimPrefix(msg, "!")
+		user_command, argument, _ := strings.Cut(msg, " ")
+		is_command = true
+		return user_command, argument, is_command
+	} else {
+		return "", "", is_command
 	}
 }
