9f5054ffff8d6d0f66bdac0b4c52937930529c13
Sambhav Soni <sambhav.soni@compunnel.com>
2026-05-05 11:18:55 +0530

    added server specific commands and checking if a guild is allowed and if command being called is allowed

--- TODO.md
+++ TODO.md
@@ -3,4 +3,7 @@ TODOS
 1. ~~Roast Command (!roast) - convert into a basic gemini wrapper with that users last 5 chat history in that channel~~
 1.1. ~~update roast with !roast <@786964843315986452> so it can be used to weaponise roasts against others as well and not only self roast~~
 1.2. update !roast discordID (optional) custom message for roast with word limit of lets say 50 words
+1.3 add cooldown and userfacing ratelimits for each user
 2. Trivia game (!quiz) on pause for now
+3. Allow specific commands for specific servers (hardcoded for now) 
+4. Tie api keys to guildids and use the respective api key
--- handlers.go
+++ handlers.go
@@ -9,15 +9,23 @@ import (
 
 var COMMAND_REGISTRY = map[string]func(*discordgo.Session, *discordgo.MessageCreate, string)string{"ai": testaiFunc,"help": helpFunc, "ping": pingFunc, "stats": statsFunc, "run":runFunc, "roast":roastFuncv2, "quiz": quizFunc } 
 
+var SERVER_ALLOWED_COMMANDS = map[string][]string{
+	"1495404097535479958": {"ai", "help", "ping", "stats", "run", "roast", "quiz"}, // SmuggyDen
+	"1340357023820415048": {"roast", "ping"}, // BadTrip Server
+	"786879993975144458" : {"roast", "ping"}, // Vibes Server (testing ground for command bifurcation)
+}
+
 func MessageHandlerv3(session *discordgo.Session, message *discordgo.MessageCreate) {
 	if message.Author.ID == session.State.User.ID {
 		return
 	}
 	fmt.Println("Message Received")
 	msg := message.Content
-	user_command, argument, is_command := messageParser(msg)
-	if is_command {
-		if cmd_func, exists := COMMAND_REGISTRY[user_command]; exists {
+	userCommand, argument, isCommand := messageParser(msg)
+	// check if command is allowed in that guild
+	isAllowed := ifGuildAllowed(message.GuildID, userCommand)
+	if isCommand && isAllowed {
+		if cmd_func, exists := COMMAND_REGISTRY[userCommand]; exists {
 			output := cmd_func(session, message, argument)
 			output = fmt.Sprintf("```%s```",output)
 			session.ChannelMessageSend(message.ChannelID, output)
@@ -35,4 +43,16 @@ func messageParser(msg string) (string, string, bool) {
 	} else {
 		return "", "", is_command
 	}
+}
+
+func ifGuildAllowed(guildID string, userCommand string) bool {
+	allowedCommands := SERVER_ALLOWED_COMMANDS[guildID]
+	isAllowed := false
+	for _, command := range allowedCommands {
+		if userCommand == command {
+		isAllowed = true
+		break
+		}
+	}
+	return isAllowed
 }
