1e8840a6308903be0c3d016fa8bd25b590185e1d
Sambhav Soni <sambhav.soni@compunnel.com>
2026-05-06 11:44:21 +0530

    bug fixed

--- cmd_functions.go
+++ cmd_functions.go
@@ -89,7 +89,6 @@ func quizFunc(session *discordgo.Session, message *discordgo.MessageCreate, arg 
 }
 
 func testaiFunc(session *discordgo.Session, message *discordgo.MessageCreate, arg string) string {
-	fmt.Printf("this is a test msg")
 	userID := message.Author.ID
 	found := false
 	WHITELISTED_IDS := getWhitelistedIDS()
